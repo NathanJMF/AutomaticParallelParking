582cb570e9bcb95bbbd638ec3e2cc7748a87b89a
Nathan <nathanfenwick@rocketmail.com>
2022-04-18 03:06:05 +0100

    Updated environment.py to now have preferred car orientation and obstacle layout. Also has an empty agent class now.

--- environment.py
+++ environment.py
@@ -7,8 +7,8 @@ class ParkingLot:
         self.obstacles = np.vstack([cars, walls])
         self.margin = 1
         self.background = np.ones((1000 + 20 * self.margin, 1000 + 20 * self.margin, 3))
-        self.background[10:1000 + 20 * self.margin:10, :] = np.array([200, 200, 200]) / 255
-        self.background[:, 10:1000 + 20 * self.margin:10] = np.array([200, 200, 200]) / 255
+        # self.background[10:1000 + 20 * self.margin:10, :] = np.array([200, 200, 200]) / 255
+        # self.background[:, 10:1000 + 20 * self.margin:10] = np.array([200, 200, 200]) / 255
         self.generate_obstacles()
 
     def generate_obstacles(self):
@@ -24,15 +24,20 @@ class ParkingLot:
         return self.background
 
 
+class Agent:
+    def __init__(self):
+        pass
+
+
 class Cars:
     def __init__(self):
         self.car_object = self.create_car_object()
         self.end = None
-        self.cars = {1: [[35, 20]], 2: [[65, 20]], 3: [[75, 20]], 4: [[95, 20]],
-                     5: [[35, 32]], 6: [[65, 32]], 7: [[75, 32]], 8: [[95, 32]],
-                     9: [[35, 44]], 10: [[65, 44]], 11: [[75, 44]], 12: [[95, 44]],
-                     13: [[35, 56]], 14: [[65, 56]], 15: [[75, 56]], 16: [[95, 56]],
-                     17: [[35, 68]], 18: [[65, 68]], 19: [[75, 68]], 20: [[95, 68]]}
+        self.cars = {1: [[15, 37]], 2: [[27, 37]], 3: [[39, 37]], 4: [[51, 37]],
+                     5: [[63, 37]], 6: [[15, 63]], 7: [[27, 63]], 8: [[39, 63]],
+                     9: [[51, 63]], 10: [[63, 63]], 11: [[15, 70]], 12: [[27, 70]],
+                     13: [[39, 70]], 14: [[51, 70]], 15: [[63, 70]], 16: [[15, 97]],
+                     17: [[27, 97]], 18: [[39, 97]], 19: [[51, 97]], 20: [[63, 97]]}
         self.car_obstacles = np.empty(0, dtype=int)
 
     def generate_cars(self, parking_spot):
@@ -47,7 +52,7 @@ class Cars:
 
     @staticmethod
     def create_car_object():
-        object_x, object_y = np.meshgrid(np.arange(-2, 2), np.arange(-4, 4))
+        object_x, object_y = np.meshgrid(np.arange(-4, 4), np.arange(-2, 2))
         car = np.dstack([object_x, object_y])
         car = car.reshape(-1, 2)
         return car
@@ -55,10 +60,8 @@ class Cars:
 
 class Walls:
     def __init__(self):
-        self.walls = [[70, i] for i in range(-5, 90)] + \
-                     [[30, i] for i in range(10, 105)] + \
-                     [[i, 10] for i in range(30, 36)] + \
-                     [[i, 90] for i in range(70, 76)]
+        self.walls = [[i, 33] for i in range(0, 70)] + \
+                     [[i, 66] for i in range(0, 70)]
         self.walls = np.array(self.walls)
 
     def get_walls(self):
