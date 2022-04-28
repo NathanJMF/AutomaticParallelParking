69e45b4253de426680f6fc1ba340def803759815
Nathan <nathanfenwick@rocketmail.com>
2022-04-28 16:25:13 +0100

    Added more helper functions in pathing.py. Added A* search algorithm to pathing_helper().

--- pathing.py
+++ pathing.py
@@ -17,8 +17,6 @@ def pathing(obstacles):
 
 def parallel_park(obstacles):
     margin = 1
-    obstacles_x = np.empty(0, dtype=int)
-    obstacles_y = np.empty(0, dtype=int)
     obstacles = obstacles + np.array([margin, margin])
     obstacles = obstacles[(obstacles[:, 0] >= 0) & (obstacles[:, 1] >= 0)]
     # obstacles = obstacles[obstacles[:, 0] >= 0 & obstacles[:, 1] >= 0]
@@ -27,6 +25,8 @@ def parallel_park(obstacles):
                                 np.array([[i, 0] for i in range(100 + 2 * margin)]),
                                 np.array([[i, 100 + 2 * margin - 1] for i in range(100 + 2 * margin)]),
                                 obstacles + np.array([margin, margin])]) * 10
+    # obstacles_x = np.empty(0, dtype=int)
+    # obstacles_y = np.empty(0, dtype=int)
     # for item in obstacles:
     #     obstacles_x = np.append(obstacles_x, item[:, 0])
     #     obstacles_y = np.append(obstacles_y, item[:, 1])
@@ -36,8 +36,12 @@ def parallel_park(obstacles):
     return mask, minimum_x, maximum_x, minimum_y, maximum_y
 
 
-def grid_pos(index, resolution, pos):
-    return index * resolution + pos
+# def grid_pos(index, resolution, pos):
+#     return index * resolution + pos
+
+
+def grid_pos(index, pos):
+    return index + pos
 
 
 def obstacle_mask(obstacles_x, obstacles_y, resolution, agent_radius):
@@ -49,9 +53,9 @@ def obstacle_mask(obstacles_x, obstacles_y, resolution, agent_radius):
     y = round((maximum_y - minimum_y) / resolution)
     mask = [[False for count in range(y)] for count in range(x)]
     for temp_x in range(x):
-        xa = grid_pos(temp_x, 1, round(min(obstacles_x)))
+        xa = grid_pos(temp_x, round(min(obstacles_x)))
         for temp_y in range(y):
-            ya = grid_pos(temp_y, 1, round(min(obstacles_y)))
+            ya = grid_pos(temp_y, round(min(obstacles_y)))
             for xb, yb in zip(obstacles_x, obstacles_y):
                 if math.hypot(xb - x, yb - y) < agent_radius:
                     mask[xa][ya] = True
@@ -59,22 +63,52 @@ def obstacle_mask(obstacles_x, obstacles_y, resolution, agent_radius):
     return mask, minimum_x, maximum_x, minimum_y, maximum_y
 
 
-def manoeuvre(x_start, y_start, x_end, y_end, mask, minimum_x, maximum_x, minimum_y, maximum_y):
-    new_x, new_y = pathing_helper(x_start + 1, y_start + 1, x_end + 1, y_end + 1, mask, minimum_x, maximum_x, minimum_y, maximum_y)
+def manoeuvre(x_start, y_start, x_end, y_end, mask, minimum_x, maximum_x, minimum_y, maximum_y, width):
+    new_x, new_y = pathing_helper(x_start + 1, y_start + 1, x_end + 1, y_end + 1, mask, minimum_x, maximum_x, minimum_y,
+                                  maximum_y, width)
 
 
-def pathing_helper(x_start, y_start, x_end, y_end, mask, minimum_x, maximum_x, minimum_y, maximum_y):
-    new_x = None
-    new_y = None
-    start = node(str(calc_xy_index(x_start, minimum_x)), str(calc_xy_index(y_start, minimum_y)), str(0.0), str(-1))
-    end = node(str(calc_xy_index(x_end, minimum_x)), str(calc_xy_index(y_end, minimum_y)), str(0.0), str(-1))
-    
-    return new_x, new_y
+def pathing_helper(x_start, y_start, x_end, y_end, mask, minimum_x, maximum_x, minimum_y, maximum_y, width):
+    # route_x = None
+    # route_y = None
+    start_set = dict()
+    end_set = dict()
+    # width = round((maximum_x - minimum_x) / 1)
+    start = node(calc_xy_index(x_start, minimum_x), calc_xy_index(y_start, minimum_y), 0.0, -1)
+    end = node(calc_xy_index(x_end, minimum_x), calc_xy_index(y_end, minimum_y), 0.0, -1)
+    start_set[calc_grid_index(start, minimum_x, minimum_y, width)] = start
+    while True:
+        if not bool(start_set):
+            print("Empty starting set")
+            break
+        current_id = min(start_set, key=lambda o: start_set[o]["cost"] + calc_heuristic(end, start_set[o]))
+        current_node = start_set[current_id]
+        if current_node["x"] == end["x"] and current_node["x"] == end["x"]:
+            end["cost"] = current_node["cost"]
+            end["index"] = current_node["index"]
+            break
+        del start_set[current_id]
+        end_set[current_id] = current_node
+        for count in range(len(motion)):
+            item = node(current_node["x"] + motion[count][0], current_node["y"] + motion[count][1], current_node["cost"] + motion[count][2], current_id)
+            node_id = calc_grid_index(item, minimum_x, minimum_y, width)
+            if not verify_node(minimum_x, minimum_y, maximum_x, maximum_y, item, mask):
+                continue
+            if node_id in end_set:
+                continue
+            if node_id not in start_set:
+                start_set[node_id] = item
+            elif start_set[node_id]["cost"] > item["cost"]:
+                start_set[node_id] = item
+            else:
+                continue
+    route_x, route_y = calc_final_path(end, end_set, minimum_x, minimum_y)
+    return route_x, route_y
 
 
 # Rename!
 def node(x, y, cost, index):
-    return str(x + "," + y + "," + cost + "," + index)
+    return {"x": x, "y": y, "cost": cost, "index": index}
 
 
 # Rename!
@@ -84,14 +118,36 @@ def calc_xy_index(coord, min_coord):
 
 # Rename!
 def calc_grid_index(current, minimum_x, minimum_y, x):
-    current = (current.x - minimum_x) + (x * (current.y - minimum_y))
+    current = (current["x"] - minimum_x) + (x * (current["y"] - minimum_y))
     return current
 
 
+# Rename!
 def calc_heuristic(a, b):
-    opposite = a.x - b.x
-    adjacent = a.y - b.y
+    opposite = a["x"] - b["x"]
+    adjacent = a["y"] - b["y"]
     return math.hypot(opposite, adjacent)
 
 
+# Rename!
+def verify_node(minimum_x, minimum_y, maximum_x, maximum_y, current, mask):
+    cx = grid_pos(current["x"], minimum_x)
+    cy = grid_pos(current["y"], minimum_y)
+    if cx >= maximum_x or cx < minimum_x or cy >= maximum_y or cy < minimum_y:
+        return False
+    if mask[current["x"]][current["y"]]:
+        return False
+    return True
 
+
+# Rename and check logic!
+def calc_final_path(end, closed, minimum_x, minimum_y):
+    final_x = [grid_pos(end["x"], minimum_x)]
+    final_y = [grid_pos(end["y"], minimum_y)]
+    index = end["index"]
+    while index != -1:
+        temp = closed[index]
+        final_x.append(grid_pos(temp["x"], minimum_x))
+        final_y.append(grid_pos(temp["y"], minimum_y))
+        index = temp["index"]
+    return final_x, final_y
